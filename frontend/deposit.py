29a571affcf2c05014b0c25c38e35b6655c6e288
Devin <dalldrit13@gmail.com>
2019-10-16 18:25:59 -0400

    deposit, withdraw and transfer almost finalized

--- frontend/deposit.py
+++ frontend/deposit.py
@@ -3,31 +3,57 @@ from frontendUtility import Modes, Status
 
 class Deposit():
 
-    def __init__(this):
+    def __init__(this, validAcctsList, mode):
+        this.mode = mode
+        this.validAcctsList = validAcctsList
         this.status = Status.OK
         this.acctNumber = None
         this.amount = None
 
     def depositIntAcct():
          x = 2
+    def getAcct(this):
+        validNum = False
+        num = None
+        while not validNum:
+            inNum = ri('Input the account number of account to deposit into: ')
+            if inNum == 'logout':
+                this.status = Status.LOGOUT
+                validNum = True
+            elif inNum == 'cancel':
+                this.status = Status.CANCEL
+                validNum = True
+            elif not inNum in this.validAcctsList:
+                print('{} is not a valid account. Please enter a valid account'.format(inNum))
+            else:
+                validNum = True
+                num = inNum
+        return num
     def getNumber(this):
         validNumber = False
+        depositTotal = 0 #need a way to determine the deposit total
         while not validNumber:
             numberString = ri('Enter a number to deposit in cents: ')
-            if numberString.isdigit == 0:
-                print("Please only enter digits")
-            number = int(numberString)
-            if number > 200000 and session.mode == Modes.ATM:
-                print("Maximum deposit of ¢200000 per transaction in ATM mode")
-            elif number > 99999999 and session.mode == Modes.TELLER:
-                print("Maximum deposit of ¢99999999 per transaction in Teller mode")
-            elif number > 0:
-                this.status = Status.OK
-            elif mode == 'machine':
-                this.status = Status.OK
+            if numberString.isdigit == True:
+                number = int(numberString)
             else:
-                string = 'Please enter either agent or machine if you wish to continue\nIf you do not wish to continue, please enter either cancel or logout'
-                if mode != '?' and mode != 'help':
-                    string = 'Unrecognized mode "{}". {}'.format(mode, string)
-                print(string)
+                number = 0
+            if numberString == "cancel":
+                this.status = Status.CANCEL
+                validNumber = True
+            elif numberString == "off":
+                this.status = Status.OFF
+                validNumber = True
+            elif numberString.isdigit == False:
+                print("Please only enter digits")
+            elif number > 200000 and this.mode == Modes.ATM:
+                print("Maximum deposit of "u'\xa2'"200000 per transaction in ATM mode")
+            elif number > 99999999 and this.mode == Modes.TELLER:
+                print("Maximum deposit of "u'\xa2'"99999999 per transaction in Teller mode")
+            elif number < 1:
+                print("Must deposit at least 1 cent")
+            elif depositTotal > 500000 and this.mode == Modes.ATM:
+                print("maximum deposit of "u'\xa2'"500000 a day")
+            else:
+                validNumber = True
         return number
--- frontend/withdraw.py
+++ frontend/withdraw.py
@@ -3,8 +3,55 @@ from frontendUtility import Modes, Status
 
 class Withdraw():
 
-    def __init__():
+    def __init__(this, validAcctsList, mode):
+        this.mode = mode
+        this.validAcctsList = validAcctsList
         this.status = Status.OK
+        this.acctNumber = None
+        this.amount = None
 
-    def withdrawFromAcct():
-        x = 2
+    def getAcct(this):
+        validNum = False
+        num = None
+        while not validNum:
+            inNum = ri('Input the account number of account to deposit into: ')
+            if inNum == 'logout':
+                this.status = Status.LOGOUT
+                validNum = True
+            elif inNum == 'cancel':
+                this.status = Status.CANCEL
+                validNum = True
+            elif not inNum in this.validAcctsList:
+                print('{} is not a valid account. Please enter a valid account'.format(inNum))
+            else:
+                validNum = True
+                num = inNum
+        return num
+    def getNumber(this):
+        validNumber = False
+        depositTotal = 0 #need a way to determine the deposit total
+        while not validNumber:
+            numberString = ri('Enter a number to deposit in cents: ')
+            if numberString.isdigit == True:
+                number = int(numberString)
+            else:
+                number = 0
+            if numberString == "cancel":
+                this.status = Status.CANCEL
+                validNumber = True
+            elif numberString == "off":
+                this.status = Status.OFF
+                validNumber = True
+            elif numberString.isdigit == False:
+                print("Please only enter digits")
+            elif number > 100000 and this.mode == Modes.ATM:
+                print("Maximum withdraw of "u'\xa2'"100000 per transaction in ATM mode")
+            elif number > 99999999 and this.mode == Modes.TELLER:
+                print("Maximum withdraw of "u'\xa2'"99999999 per transaction in Teller mode")
+            elif number < 1:
+                print("Must withdraw at least 1 cent")
+            elif depositTotal > 500000 and this.mode == Modes.ATM:
+                print("maximum withdrawal of "u'\xa2'"500000 a day")
+            else:
+                validNumber = True
+        return number
