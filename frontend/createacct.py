5e310ab398e8b547f6a54509522b8f7bc93b731d
Devin <dalldrit13@gmail.com>
2019-10-17 19:47:02 -0400

    fixed error made in push

--- frontend/createacct.py
+++ frontend/createacct.py
@@ -1,17 +1,19 @@
 """
 createacct.py
 
-This file contains the class and all of its methods to handle the frontend steps to creating a new account.
+This file contains the class and all of its methods to handle the frontend
+steps to creating a new account.
 """
 from frontendUtility import requiredInput as ri
 from frontendUtility import Status
 
+
 class CreateAcct():
     """
     Class that handles the createacct command.
-    Requires a valid accounts list to be given during intialization.
-    This function does not modify the validAcctsList variable as the creation of a new account can only be validated
-    by the back office.
+
+    @param validAcctsList The dictionary the contains the list of valid
+                          accounts
     """
     def __init__(this, validAcctsList):
         this.validAcctsList = validAcctsList
@@ -22,76 +24,85 @@ class CreateAcct():
     def createNewAccount(this):
         """
         Gets the new account number and name.
-        If logout or cancel are given as the number or name then this will return and the status of this object will
-        indicate what caused the return.
+        If logout or cancel are given as the number or name then this will
+        return and the status of this object will indicate what caused the
+        return.
         """
-        num = this._getNewAcctNum()
+        num = this.__getNewAcctNum()
         if this.status != Status.OK:
             return
-        name = this._getNewAcctName()
+        name = this.__getNewAcctName()
         if this.status != Status.OK:
             return
         this.newAcctNum = num
         this.newAcctName = name
 
-
-    def _getNewAcctNum(this):
+    def __getNewAcctNum(this):
         """
         Gets the requested account number of the new account.
-        If logout or cancel is given as input this funciton will return and the status of this object will indicate
-        what caused the return.
+        If logout or cancel is given as input this funciton will return and
+        the status of this object will indicate what caused the return.
 
-        Ensures that the given account number is in valid format as described by the requirements.
+        Ensures that the given account number is in valid format as described
+        by the requirements.
         """
         validNum = False
         num = None
         while not validNum:
             inNum = ri('Input the account number for the new account: ')
-            if inNum == 'logout':
+            if inNum == 'logout' or inNum == 'off':
                 this.status = Status.LOGOUT
                 validNum = True
             elif inNum == 'cancel':
                 this.status = Status.CANCEL
                 validNum = True
             elif not inNum.isdigit():
-                print('Account numbers must be comprised of only digits. Please choose a different account number.')
+                print('Account numbers must be comprised of only digits. '
+                      'Please choose a different account number.')
             elif len(inNum) != 7:
-                print('Account numbers must be exactly 7 digits in length. Please choose a different account number.')
+                print('Account numbers must be exactly 7 digits in length. '
+                      'Please choose a different account number.')
             elif inNum == '0000000':
-                print('0000000 is a reserved account number. Please choose a different account number.')
+                print('0000000 is a reserved account number. Please choose a '
+                      'different account number.')
             elif inNum[0] == '0':
-                print('Account numbers must not begin with a "0". Please choose a different account number.')
-            elif inNum in this.validAcctsList:
-                print('{} is already in use. Please choose a different account number.'.format(inNum))
+                print('Account numbers must not begin with a "0". Please '
+                      'choose a different account number.')
+            elif inNum in this.validAcctsList.keys():
+                print('{} is already in use. Please choose a different account'
+                      ' number.'.format(inNum))
             else:
                 validNum = True
                 num = inNum
         return num
 
-    def _getNewAcctName(this):
+    def __getNewAcctName(this):
         """
         Gets the requested account name of the new account.
-        If logout or cancel is given as input this funciton will return and the status of this object will indicate
-        what caused the return.
+        If logout or cancel is given as input this funciton will return and
+        the status of this object will indicate what caused the return.
 
-        Ensures that the given account name is in valid format as described by the requirements.
+        Ensures that the given account name is in valid format as described by
+        the requirements.
         """
         validName = False
         name = None
         while not validName:
-            inName = ri('Input the account name for the new account: ', toLower=False)
-            if inName == 'logout':
+            inName = ri('Input the account name for the new account: ',
+                        toLower=False)
+            if inName == 'logout' or inName == 'off':
                 this.status = Status.LOGOUT
                 validName = True
             elif inName == 'cancel':
                 this.status = Status.CANCEL
                 validName = True
             elif len(inName) < 3 or len(inName) > 30:
-                print('Account names must be between 3 and 30 characters. Please choose a different account name.')
+                print('Account names must be between 3 and 30 characters. '
+                      'Please choose a different account name.')
             elif not inName.replace(' ', '').isalnum():
-                print('Account names must only contain alphanumeric characters and spaces. Please choose a different account name.')
+                print('Account names must only contain alphanumeric characters'
+                      ' and spaces. Please choose a different account name.')
             else:
                 validName = True
                 name = inName
         return name
-    
--- frontend/login.py
+++ frontend/login.py
@@ -1,15 +1,18 @@
 """
 login.py
 
-This file contains the class and all of its methods to handle logging in to the Quinterac banking system.
+This file contains the class and all of its methods to handle logging in to
+the Quinterac banking system.
 """
 from frontendUtility import requiredInput as ri
 from frontendUtility import Modes, Status
 
+
 class Login():
     """
     Class that handles logging in.
-    If a cancel or logout is given as input while getting the mode, the classes status will indicate what has happened.
+    If a cancel or logout is given as input while getting the mode, the
+    classes status will indicate what has happened.
     """
     def __init(this):
         this.status = Status.OK
@@ -18,8 +21,10 @@ class Login():
         """
         Function to ask get the requested banking system mode from the user.
 
-        Logout and cancel may also be given as input rather than completing the login.
-        Inputs of "?" and "help" can be used to prompt the user of possible valid options.
+        Logout and cancel may also be given as input rather than completing
+        the login.
+        Inputs of "?" and "help" can be used to prompt the user of possible
+        valid options.
         """
         validMode = False
         retMode = Modes.NA
@@ -42,7 +47,9 @@ class Login():
                 retMode = Modes.NA
                 this.status = Status.LOGOUT
             else:
-                string = 'Please enter either agent or machine if you wish to continue\nIf you do not wish to continue, please enter either cancel or logout'
+                string = 'Please enter either agent or machine if you wish to '\
+                         'continue\nIf you do not wish to continue, please '\
+                         'enter either cancel or logout'
                 if mode != '?' and mode != 'help':
                     string = 'Unrecognized mode "{}". {}'.format(mode, string)
                 print(string)
@@ -50,20 +57,24 @@ class Login():
 
     def getValidAccts(this, validAcctsFile):
         """
-        Function to get the list of valid accounts from the valid accounts list file.
+        Function to get the list of valid accounts from the valid accounts
+        list file.
         """
-        accts = []
+        accts = {}
         with open(validAcctsFile, 'r') as f:
             tempAccts = f.readlines()
             if '0000000\n' not in tempAccts:
-                print('Error processing {}, no special end account "0000000"'.format(validAcctsFile))
+                print('Error processing {}, no special end account '
+                      '"0000000"'.format(validAcctsFile))
             else:
                 for acct in tempAccts:
                     acct = acct.replace('\n', '')
                     if acct == '0000000':
                         break
                     elif acct.isdigit() and len(acct) == 7:
-                        accts.append(acct)
+                        accts[acct] = {'withdraw': 0, 'deposit': 0,
+                                       'transfer': 0, }
                     else:
-                        print('Some accounts in {} are not in valid format'.format(validAcctsFile))
+                        print('Some accounts in {} are not in valid '
+                              'format'.format(validAcctsFile))
         return accts
