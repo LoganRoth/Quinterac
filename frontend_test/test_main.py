e383b76777d15d3bce6d515aa01d363ff9aabef4
Devin <dalldrit13@gmail.com>
2019-10-31 19:13:35 -0400

    fixed errors

--- frontend_test/test_main.py
+++ frontend_test/test_main.py
@@ -137,7 +137,7 @@ def test_r4t1(capsys):
          accts_file='validAccountsList.txt',
          test_id='R4T1'
      )
-	 
+
 def test_r4t2(capsys):
      """
      Testing r4t2. All required information stored in folder r4.
@@ -150,7 +150,7 @@ def test_r4t2(capsys):
          accts_file='validAccountsList.txt',
          test_id='R4T2'
      )
-	 
+
 def test_r4t3(capsys):
      """
      Testing r4t3. All required information stored in folder r4.
@@ -163,7 +163,7 @@ def test_r4t3(capsys):
          accts_file='validAccountsList.txt',
          test_id='R4T3'
      )
-	 
+
 def test_r4t4(capsys):
      """
      Testing r4t4. All required information stored in folder r4.
@@ -176,7 +176,7 @@ def test_r4t4(capsys):
          accts_file='validAccountsList.txt',
          test_id='R4T4'
      )
-	 
+
 def test_r4t5(capsys):
      """
      Testing r4t5. All required information stored in folder r4.
@@ -189,7 +189,7 @@ def test_r4t5(capsys):
          accts_file='validAccountsList.txt',
          test_id='R4T5'
      )
-	 
+
 def test_r4t6(capsys):
      """
      Testing r4t6. All required information stored in folder r4.
@@ -202,7 +202,7 @@ def test_r4t6(capsys):
          accts_file='validAccountsList.txt',
          test_id='R4T6'
      )
-	 
+
 def test_r4t7(capsys):
      """
      Testing r4t7. All required information stored in folder r4.
@@ -215,7 +215,7 @@ def test_r4t7(capsys):
          accts_file='validAccountsList.txt',
          test_id='R4T7'
      )
-	 
+
 def test_r4t8(capsys):
      """
      Testing r4t8. All required information stored in folder r4.
@@ -228,7 +228,7 @@ def test_r4t8(capsys):
          accts_file='validAccountsList.txt',
          test_id='R4T8'
      )
-	 
+
 def test_r4t9(capsys):
      """
      Testing r4t9. All required information stored in folder r4.
@@ -240,84 +240,6 @@ def test_r4t9(capsys):
          test_set='logout',
          accts_file='validAccountsList.txt',
          test_id='R4T9'
-     )
-	 
-def test_r4t10(capsys):
-     """
-     Testing r4t10. All required information stored in folder r4.
-
-     @param capsys Object created by pytest to capture stdout and stderr
-     """
-     helper(
-         capsys=capsys,
-         test_set='logout',
-         accts_file='validAccountsList.txt',
-         test_id='R4T10'
-     )
-	 
-def test_r4t11(capsys):
-     """
-     Testing r4t1. All required information stored in folder r4.
-
-     @param capsys Object created by pytest to capture stdout and stderr
-     """
-     helper(
-         capsys=capsys,
-         test_set='logout',
-         accts_file='validAccountsList.txt',
-         test_id='R4T11'
-     )
-	 
-def test_r4t12(capsys):
-     """
-     Testing r4t12. All required information stored in folder r4.
-
-     @param capsys Object created by pytest to capture stdout and stderr
-     """
-     helper(
-         capsys=capsys,
-         test_set='logout',
-         accts_file='validAccountsList.txt',
-         test_id='R4T12'
-     )
-	 
-def test_r4t13(capsys):
-     """
-     Testing r4t13. All required information stored in folder r4.
-
-     @param capsys Object created by pytest to capture stdout and stderr
-     """
-     helper(
-         capsys=capsys,
-         test_set='logout',
-         accts_file='validAccountsList.txt',
-         test_id='R4T13'
-     )
-	 
-def test_r4t14(capsys):
-     """
-     Testing r4t14. All required information stored in folder r4.
-
-     @param capsys Object created by pytest to capture stdout and stderr
-     """
-     helper(
-         capsys=capsys,
-         test_set='logout',
-         accts_file='validAccountsList.txt',
-         test_id='R4T14'
-     )
-	 
-def test_r4t15(capsys):
-     """
-     Testing r4t15. All required information stored in folder r4.
-
-     @param capsys Object created by pytest to capture stdout and stderr
-     """
-     helper(
-         capsys=capsys,
-         test_set='logout',
-         accts_file='validAccountsList.txt',
-         test_id='R4T15'
      )
 
 
