0dc363aa58bc23c28c0e516af26bbdad85673ec2
Devin <dalldrit13@gmail.com>
2019-11-01 13:54:00 -0400

    fixed all errors

--- frontend_test/test_main.py
+++ frontend_test/test_main.py
@@ -107,19 +107,78 @@ R3: Valid Accounts List File
 ============================================================================"""
 
 
-# def test_r3t1(capsys):
-#     """
-#     Testing r3t1. All required information stored in folder r3.
+def test_r3t1(capsys):
+    """
+    Testing r3t1. All required information stored in folder r3.
 
-#     @param capsys Object created by pytest to capture stdout and stderr
-#     """
-#     helper(
-#         capsys=capsys,
-#         test_set='validAccountsListFile',
-#         accts_file='validAccountsList.txt',
-#         test_id='R3T1'
-#     )
+    @param capsys Object created by pytest to capture stdout and stderr
+    """
+    helper(
+        capsys=capsys,
+        test_set='validAccountsListFile',
+        accts_file='tooLongFile.txt',
+        test_id='R3T1'
+    )
+def test_r3t2(capsys):
+    """
+    Testing r3t2. All required information stored in folder r3.
 
+    @param capsys Object created by pytest to capture stdout and stderr
+    """
+    helper(
+        capsys=capsys,
+        test_set='validAccountsListFile',
+        accts_file='tooShortFile.txt',
+        test_id='R3T2'
+    )
+def test_r3t3(capsys):
+    """
+    Testing r3t3. All required information stored in folder r3.
+
+    @param capsys Object created by pytest to capture stdout and stderr
+    """
+    helper(
+        capsys=capsys,
+        test_set='validAccountsListFile',
+        accts_file='nonDigitFile.txt',
+        test_id='R3T3'
+    )
+def test_r3t4(capsys):
+    """
+    Testing r3t4. All required information stored in folder r3.
+
+    @param capsys Object created by pytest to capture stdout and stderr
+    """
+    helper(
+        capsys=capsys,
+        test_set='validAccountsListFile',
+        accts_file='zeroStartFile.txt',
+        test_id='R3T4'
+    )
+def test_r3t5(capsys):
+    """
+    Testing r3t5. All required information stored in folder r3.
+
+    @param capsys Object created by pytest to capture stdout and stderr
+    """
+    helper(
+        capsys=capsys,
+        test_set='validAccountsListFile',
+        accts_file='acctsAfterSpecialFile.txt',
+        test_id='R3T5'
+    )
+def test_r3t6(capsys):
+    """
+    Testing r3t6. All required information stored in folder r3.
+
+    @param capsys Object created by pytest to capture stdout and stderr
+    """
+    helper(
+        capsys=capsys,
+        test_set='validAccountsListFile',
+        accts_file='noSpecialFile.txt',
+        test_id='R3T6'
+    )
 
 """============================================================================
 R4: Logout
