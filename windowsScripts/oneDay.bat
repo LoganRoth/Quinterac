7f25a40c72ff2d3458d9196de019a7af6bf33871
Devin <dalldrit13@gmail.com>
2019-12-07 12:33:38 -0500

    added reset to beginning of scripts so only 1 needed to run

--- windowsScripts/oneDay.bat
+++ windowsScripts/oneDay.bat
@@ -5,6 +5,16 @@
 
 cd ..
 
+: > oneDayTest\summary1.txt
+: > oneDayTest\summary2.txt
+: > oneDayTest\summary3.txt
+: > oneDayTest\summary.txt
+echo '1234567
+7654321
+0000000' > validAcctsListFile.txt
+echo '7654321 000 aaa
+1234567 20000 cba' > oneDayTest\masterAccts.txt
+
 echo 'login
 machine
 deposit
--- windowsScripts/oneWeek.bat
+++ windowsScripts/oneWeek.bat
@@ -5,6 +5,13 @@
 
 cd ..
 
+: > oneWeekTest\summary1.txt
+: > oneWeekTest\summary2.txt
+: > oneWeekTest\summary3.txt
+: > oneDayTest\summary.txt
+echo '0000000' > validAcctsListFile.txt
+: > oneWeekTest\masterAccts.txt
+
 #Day 1___________________________________________
 echo 'login
 agent
